c4c625ac94d4f11ffe8f4da3c079cdf5d5d29523
Tom Nielsen <tanielsen@gmail.com>
2020-09-03 10:10:12 +0100

    card footer

--- index.js
+++ index.js
@@ -19,25 +19,25 @@ const {
   mkTag,
   button,
   nav,
-  img
+  img,
 } = require("@saltcorn/markup/tags");
 const renderLayout = require("@saltcorn/markup/layout");
 const { renderForm, link } = require("@saltcorn/markup");
 
-const subItem = currentUrl => item =>
+const subItem = (currentUrl) => (item) =>
   li(
     item.link
       ? a(
           {
             class: ["dropdown-item", active(currentUrl, item) && "active"],
-            href: text(item.link)
+            href: text(item.link),
           },
           item.label
         )
       : span({ class: "dropdown-header" }, item.label)
   );
 
-const labelToId = item => text(item.label.replace(" ", ""));
+const labelToId = (item) => text(item.label.replace(" ", ""));
 
 const logit = (x, s) => {
   if (s) console.log(s, x);
@@ -47,7 +47,7 @@ const logit = (x, s) => {
 const active = (currentUrl, item) =>
   (item.link && currentUrl.startsWith(item.link)) ||
   (item.subitems &&
-    item.subitems.some(si => si.link && currentUrl.startsWith(si.link)));
+    item.subitems.some((si) => si.link && currentUrl.startsWith(si.link)));
 
 const arrowDown = `<svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
 <path stroke="none" d="M0 0h24v24H0z"/>
@@ -57,11 +57,11 @@ const arrowDown = `<svg xmlns="http://www.w3.org/2000/svg" class="icon" width="2
 <line x1="12" y1="12" x2="4" y2="7.5" />
 <line x1="16" y1="5.25" x2="8" y2="9.75" /></svg>`;
 
-const sideBarItem = currentUrl => item => {
+const sideBarItem = (currentUrl) => (item) => {
   const is_active = active(currentUrl, item);
   return li(
     {
-      class: ["nav-item", is_active && "active", item.subitems && "dropdown"]
+      class: ["nav-item", is_active && "active", item.subitems && "dropdown"],
     },
     item.link
       ? a({ class: ["nav-link"], href: text(item.link) }, text(item.label))
@@ -73,7 +73,7 @@ const sideBarItem = currentUrl => item => {
               href: "#",
               "data-toggle": "dropdown",
               role: "button",
-              "aria-expanded": "false"
+              "aria-expanded": "false",
             },
             //i({ class: "fas fa-fw fa-wrench" }),
             //span({class: "nav-link-icon d-md-none d-lg-inline-block"},arrowDown),
@@ -81,20 +81,20 @@ const sideBarItem = currentUrl => item => {
           ),
           ul(
             {
-              class: "dropdown-menu"
+              class: "dropdown-menu",
             },
 
             item.subitems.map(subItem(currentUrl))
-          )
+          ),
         ]
       : span({ class: "nav-link" }, text(item.label))
   );
 };
 
-const sideBarSection = currentUrl => section => [
+const sideBarSection = (currentUrl) => (section) => [
   //section.section &&
   //  li({ class: "nav-header text-uppercase" }, section.section),
-  section.items.map(sideBarItem(currentUrl)).join("")
+  section.items.map(sideBarItem(currentUrl)).join(""),
 ];
 
 const header_sections = (brand, sections, currentUrl) =>
@@ -107,7 +107,7 @@ const header_sections = (brand, sections, currentUrl) =>
           class: "navbar-toggler",
           type: "button",
           "data-toggle": "collapse",
-          "data-target": "#navbar-menu"
+          "data-target": "#navbar-menu",
         },
         span({ class: "navbar-toggler-icon" })
       ),
@@ -115,13 +115,13 @@ const header_sections = (brand, sections, currentUrl) =>
         {
           href: ".",
           class:
-            "navbar-brand navbar-brand-autodark d-none-navbar-horizontal pr-0 pr-md-3"
+            "navbar-brand navbar-brand-autodark d-none-navbar-horizontal pr-0 pr-md-3",
         },
         brand.logo &&
           img({
             src: brand.logo,
             alt: "Logo",
-            class: "navbar-brand-image mx-1"
+            class: "navbar-brand-image mx-1",
           }),
         brand.name
       ),
@@ -129,9 +129,9 @@ const header_sections = (brand, sections, currentUrl) =>
       div(
         { class: "navbar-nav flex-row order-md-last" },
         sections
-          .filter(s => s.section === "User")
-          .map(s =>
-            s.items.map(item =>
+          .filter((s) => s.section === "User")
+          .map((s) =>
+            s.items.map((item) =>
               div(
                 { class: "nav-item" },
                 a(
@@ -154,10 +154,10 @@ const header_sections = (brand, sections, currentUrl) =>
           { class: "container-xl" },
           ul(
             {
-              class: "navbar-nav"
+              class: "navbar-nav",
             },
             sections
-              .filter(s => s.section !== "User")
+              .filter((s) => s.section !== "User")
               .map(sideBarSection(currentUrl))
           )
         )
@@ -172,12 +172,13 @@ const blockDispatch = {
       h2({ class: "page-title" }, title),
       blurb && p({ class: "mb-0 text-gray-800" }, blurb)
     ),
-  card: ({ title, contents }, go) =>
+  card: ({ title, contents, footer }, go) =>
     div(
       { class: "card" },
       title &&
         div({ class: "card-header" }, h3({ class: "card-title" }, text(title))),
-      div({ class: "card-body" }, go(contents))
+      div({ class: "card-body" }, go(contents)),
+      footer && div({ class: "card-footer" }, go(footer))
     ),
   footer: ({ contents }) =>
     div(
@@ -195,7 +196,7 @@ const blockDispatch = {
         div(
           {
             class:
-              "row h-100 align-items-center justify-content-center text-center"
+              "row h-100 align-items-center justify-content-center text-center",
           },
           div(
             { class: "col-lg-10 align-self-end" },
@@ -215,7 +216,7 @@ const blockDispatch = {
           )
         )
       )
-    )
+    ),
 };
 const renderBody = (title, body) =>
   renderLayout({
@@ -225,12 +226,12 @@ const renderBody = (title, body) =>
         ? {
             above: [
               { type: "pageHeader", title },
-              { type: "card", contents: body }
-            ]
+              { type: "card", contents: body },
+            ],
           }
-        : body
+        : body,
   });
-const wrapIt=(bodyAttr, headers, title, body) => `<!doctype html>
+const wrapIt = (bodyAttr, headers, title, body) => `<!doctype html>
 <html lang="en">
   <head>
     <!-- Required meta tags -->
@@ -244,12 +245,12 @@ const wrapIt=(bodyAttr, headers, title, body) => `<!doctype html>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/tabler@1.0.0-alpha.7/dist/css/tabler.min.css" 
           integrity="sha256-pqiuW1qcWlMprs8p96Yvsxp5Cq9C8duKeqWJInj7mJ8=" crossorigin="anonymous">
     ${headers
-      .filter(h => h.css)
-      .map(h => `<link href="${h.css}" rel="stylesheet">`)
+      .filter((h) => h.css)
+      .map((h) => `<link href="${h.css}" rel="stylesheet">`)
       .join("")}
     ${headers
-      .filter(h => h.headerTag)
-      .map(h => h.headerTag)
+      .filter((h) => h.headerTag)
+      .map((h) => h.headerTag)
       .join("")}
     <title>${text(title)}</title>
   </head>
@@ -266,9 +267,9 @@ const wrapIt=(bodyAttr, headers, title, body) => `<!doctype html>
     crossorigin="anonymous"></script>
 
   ${headers
-    .filter(h => h.script)
+    .filter((h) => h.script)
     .map(
-      h =>
+      (h) =>
         `<script src="${h.script}" ${
           h.integrity
             ? `integrity="${h.integrity}" crossorigin="anonymous"`
@@ -282,9 +283,9 @@ const wrapIt=(bodyAttr, headers, title, body) => `<!doctype html>
       }
     </style>
 </body>
-</html>`
+</html>`;
 
-const renderAuthLinks = authLinks => {
+const renderAuthLinks = (authLinks) => {
   var links = [];
   if (authLinks.login)
     links.push(link(authLinks.login, "Already have an account? Login!"));
@@ -292,10 +293,13 @@ const renderAuthLinks = authLinks => {
   if (authLinks.signup)
     links.push(link(authLinks.signup, "Create an account!"));
   if (links.length === 0) return "";
-  else return links.map(l => div({ class: "text-center text-muted" }, l)).join("");
+  else
+    return links
+      .map((l) => div({ class: "text-center text-muted" }, l))
+      .join("");
 };
 
-const formModify = form => {
+const formModify = (form) => {
   form.formStyle = "vert";
   form.submitButtonClass = "btn-primary btn-user btn-block";
   return form;
@@ -316,15 +320,19 @@ const authWrap = ({
   brand,
   headers,
   csrfToken,
-  authLinks
+  authLinks,
 }) =>
-  wrapIt('', headers, title, `<div class="page">
+  wrapIt(
+    "",
+    headers,
+    title,
+    `<div class="page">
   <div class="page-single">
     <div class="container">
       <div class="row">
         <div class="col col-login mx-auto">
-        ${alerts.map(a => alert(a.type, a.msg)).join("")}
-        ${authBrand( brand)}
+        ${alerts.map((a) => alert(a.type, a.msg)).join("")}
+        ${authBrand(brand)}
           <div class="card">
             <div class="card-body p-5">
               <div class="card-title">${title}</div>
@@ -355,28 +363,24 @@ const authWrap = ({
     padding: 1rem 0;
 }
   </style>
-</div>`)
+</div>`
+  );
 
-
-const wrap = ({
-  title,
-  menu,
-  brand,
-  alerts,
-  currentUrl,
-  body,
-  headers
-}) => wrapIt('class="antialiased"', headers, title, 
-`<div id="page">
+const wrap = ({ title, menu, brand, alerts, currentUrl, body, headers }) =>
+  wrapIt(
+    'class="antialiased"',
+    headers,
+    title,
+    `<div id="page">
         ${header_sections(brand, menu, currentUrl)}
         <div class="content">
             <div class="container-xl">
-              ${alerts.map(a => alert(a.type, a.msg)).join("")}
+              ${alerts.map((a) => alert(a.type, a.msg)).join("")}
               ${renderBody(title, body)}
             </div>
         </div>
     </div>`
-);
+  );
 
 const alert = (type, s) => {
   //console.log("alert", type, s,s.length)
